c64c58e99ffc90db579be6f4ceaded668852c78b
danielkar <daniil-karachun@mail.ru>
2020-03-15 23:26:33 +0300

    registerlogin

--- shop/views.py
+++ shop/views.py
@@ -35,14 +35,14 @@ def product_detail(request, id, slug):
 		id=id,
 		slug=slug,
 		available=True,
-    )
-    cart_product_form = CartAddProductForm()
+        )
+      cart_product_form = CartAddProductForm()
 
 	context = {
 		'product': product,
         'cart_product_form': cart_product_form,
 	}
-	return render(request, 'shop/product/detail.html', context)
+	return render(request, 'shop/product/detail.html', context)    
 
 def index(request):
     return render(request,'shop/base.html')
