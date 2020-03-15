f7e51df3433e9a1365191b989a2b666f5025c85c
danielkar <daniil-karachun@mail.ru>
2020-03-16 00:00:15 +0300

    fix exception

--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,6 @@ app_name = 'cart'
 
 urlpatterns = [
 	path('', cart_detail, name="cart_detail"),
-	path('add/<product_id>/', cart_add, name='cart_add'),
-	path('remove/<product_id/>', cart_remove, name='cart_remove'),
+	path('add/<product_id>/', cart_add, name="cart_add"),
+	path('remove/<product_id>/', cart_remove, name="cart_remove"),
 ]
